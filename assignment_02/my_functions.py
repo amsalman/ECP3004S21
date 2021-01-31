8f5ababd7d49c0ccdb6454d67177eb871e916850
amsalman <77473530+amsalman@users.noreply.github.com>
2021-01-31 10:57:14 -0500

    Update my_functions.py
    Solution for Assignment 2 Question 1 Example 1

--- assignment_02/my_functions.py
+++ assignment_02/my_functions.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-##################################################
-#
+Created on Sun Jan 31 09:20:51 2021
+
+@author: Hadis
+"""
+
 # ECP 3004: Python for Business Analytics
 #
-# Name: 
+# Name: Hadi Salman
 #
-# Date:
+# Date:1/31/2021
 # 
 ##################################################
 #
@@ -14,7 +17,7 @@
 # Function Definitions
 #
 ##################################################
-"""
+
 
 
 ##################################################
@@ -30,19 +33,22 @@
 
 # Exercise 1
 
-def average(num1: float, num2: float) -> float:
-    """Return the average of num1 and num2.
 
-    >>> average(10,20)
-    15.0
-    >>> average(2.5, 3.0)
-    2.75
-    """
+#defining average function
+def average(a,b):
+    return (a+b)/2
+#taking 2 numbers as input
+x=float(input("Enter first number"))
+y=float(input("Enter second number"))
 
-    return num1 + num2 / 2
-
+print("The average is", average(x,y))
 
 
+#Example 1 (10,20)
+
+#Example 2 (15.5,30)
+
+#Example 3 (13.8,27.4)
 # Define the rest of your functions for Exercises 2-6.
  
 
@@ -59,7 +65,7 @@ def average(num1: float, num2: float) -> float:
 # Test the examples and print the results. 
 
 
-print("#" + 50*"-")
+print("20" + 50*"-")
 print("Testing my Examples for Exercise 1.")
 
 print("#" + 50*"-")
@@ -78,6 +84,7 @@ print("Got: " + str(average(2.5, 3.0)))
 
 print("#" + 50*"-")
 print("Exercise 1, Example 3:")
+
 # ...
 
 print("#" + 50*"-")
@@ -88,6 +95,13 @@ print("Testing my Examples for Exercise 2.")
 
 # Continue with the rest of your examples.
 # Test all functions with three examples each. 
+#Example 1
+print("#" + 50*"-")
+print("Exercise 1, Example 2:")
+print("Evaluating average(2.5, 3.0)")
+print("Expected: " + str(2.75))
+print("Got: " + str(average(2.5, 3.0)))
+
 
 # Choose good examples that will test interesting cases. 
 # Make sure they all work correctly. 
